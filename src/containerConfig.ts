ce28f577b089bd437089aa434907c81b376da737
NatalieShaked <shakednatalie@gmail.com>
2023-07-30 14:50:12 +0300

    feat(configurations): add helm chart for repo
    * feat(configurations): add support for prettyPrint option

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -4,20 +4,20 @@ import { getOtelMixin, Metrics } from '@map-colonies/telemetry';
 import jsLogger, { LoggerOptions } from '@map-colonies/js-logger';
 import { trace } from '@opentelemetry/api';
 import { metrics } from '@opentelemetry/api-metrics';
-import { Services } from './common/constants';
+import { Services, SERVICE_NAME } from './common/constants';
 import { tracing } from './common/tracing';
 
 function registerExternalValues(): void {
   const loggerConfig = config.get<LoggerOptions>('telemetry.logger');
-  const logger = jsLogger({ ...loggerConfig, mixin: getOtelMixin() });
+  const logger = jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, mixin: getOtelMixin() });
   container.register(Services.CONFIG, { useValue: config });
   container.register(Services.LOGGER, { useValue: logger });
 
   const otelMetrics = new Metrics();
   otelMetrics.start();
-  container.register(Services.METER, { useValue: metrics.getMeter('id-merger') });
+  container.register(Services.METER, { useValue: metrics.getMeter(SERVICE_NAME) });
 
-  const tracer = trace.getTracer('id-merger');
+  const tracer = trace.getTracer(SERVICE_NAME);
   container.register(Services.TRACER, { useValue: tracer });
 
   container.register('onSignal', {
