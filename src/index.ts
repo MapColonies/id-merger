dbfbd4c3353b1a0fb689a6b7c3145e907d678ad1
Niv Greenstein <greenstein9@gmail.com>
2025-01-14 17:22:58 +0200

    chore: updated according to ts-server-boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-/* eslint-disable import/first */
-// this import must be called before the first import of tsyring
+// this import must be called before the first import of tsyringe
 import 'reflect-metadata';
-import './common/tracing';
 import { createServer } from 'http';
 import { createTerminus } from '@godaddy/terminus';
 import { Logger } from '@map-colonies/js-logger';
 import { container } from 'tsyringe';
-import config from 'config';
-import { DEFAULT_SERVER_PORT, Services } from './common/constants';
-
+import { SERVICES } from '@common/constants';
+import { ConfigType } from '@common/config';
 import { getApp } from './app';
 
-interface IServerConfig {
-  port: string;
-}
+void getApp()
+  .then(([app]) => {
+    const logger = container.resolve<Logger>(SERVICES.LOGGER);
+    const config = container.resolve<ConfigType>(SERVICES.CONFIG);
+    const port = config.get('server.port');
+    const stubHealthCheck = async (): Promise<void> => Promise.resolve();
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthCheck }, onSignal: container.resolve('onSignal') });
 
-const serverConfig = config.get<IServerConfig>('server');
-const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
-
-const app = getApp();
-
-const logger = container.resolve<Logger>(Services.LOGGER);
-const stubHealthcheck = async (): Promise<void> => Promise.resolve();
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const server = createTerminus(createServer(app), { healthChecks: { '/liveness': stubHealthcheck }, onSignal: container.resolve('onSignal') });
-
-server.listen(port, () => {
-  logger.info(`app started on port ${port}`);
-});
+    server.listen(port, () => {
+      logger.info(`app started on port ${port}`);
+    });
+  })
+  .catch((error: Error) => {
+    console.error('😢 - failed initializing the server');
+    console.error(error);
+    process.exit(1);
+  });
